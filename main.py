607f9c8c62923f4e945d39c6c38725aad350d0d1
Hannah Liu <readlife2000@gmail.com>
2023-09-14 15:06:14 -0400

    Updated background

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ pygame.init()
 screen = pygame.display.set_mode((1080, 620))
 
 pygame.display.set_caption("Undead Uprising")
-icon = pygame.image.load('test_char.png')
-pygame.display.set_icon(icon)
+# icon = pygame.image.load('test_char.png')
+# pygame.display.set_icon(icon)
 # icon = pygame.image.load('')
 # pygame.display.set_icon(icon)
 
 # Background
-bg = pygame.image.load('test_bg.png')
+bg = pygame.image.load('bg.jpg')
 
 # Character
-charImg = pygame.image.load('test_char.png')
+# charImg = pygame.image.load('test_char.png')
 charX = 200
 charY = 100
 
