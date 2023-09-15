240142ffcf1943df8f5b5080d33a170bec15ebe5
Hannah Liu <readlife2000@gmail.com>
2023-09-14 20:37:46 -0400

    Updated default character

--- main.py
+++ main.py
@@ -4,25 +4,28 @@ pygame.init()
 screen = pygame.display.set_mode((1080, 620))
 
 pygame.display.set_caption("Undead Uprising")
-# icon = pygame.image.load('test_char.png')
-# pygame.display.set_icon(icon)
-# icon = pygame.image.load('')
-# pygame.display.set_icon(icon)
+icon = pygame.image.load('test_char.png')
+pygame.display.set_icon(icon)
 
 # Background
 bg = pygame.image.load('bg.jpg')
 
 # Character
-# charImg = pygame.image.load('test_char.png')
-charX = 200
-charY = 100
+charImg = pygame.image.load('char_blue.png')
+charX = 500
+charY = 300
+
+def player():
+    screen.blit(charImg, (charX, charY))
 
 running = True
 while running:
-    screen.fill((0, 0, 0))
-    # Bg Image
-    screen.blit(bg, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    screen.fill((0, 0, 0))
+    # Bg Image
+    screen.blit(bg, (0, 0))
+    player()
     pygame.display.update()
