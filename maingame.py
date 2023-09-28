1b8351d2de780687df6abcc16eb94e190c3cc8ac
Hannah Liu <readlife2000@gmail.com>
2023-09-28 14:24:05 -0400

    Added current version
    - Organized code
    - Built start menu

---
+++ maingame.py
@@ -0,0 +1,30 @@
+import pygame
+import sys
+
+import startpage
+
+pygame.init()
+
+# Screen Settings
+screen = pygame.display.set_mode((1080, 620))  # resizable not added
+screen_w, screen_h = pygame.display.get_surface().get_size()
+
+# Game Details
+pygame.display.set_caption("Undead Uprising")
+icon = pygame.image.load('test_char.png')
+pygame.display.set_icon(icon)
+big_logo = pygame.image.load('UNDEAD UPRISING.png')
+
+game_state = "start_menu"
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+            running = False
+    screen.fill((0, 0, 0))
+    if game_state == "start_menu":
+        startpage.display_start_menu()
+    if game_state == 'game_play':
+        print()
+    if game_state == '':
+        game_state = "hi"
---
+++ startpage.py
@@ -0,0 +1,74 @@
+import pygame
+import sys
+
+screen = pygame.display.set_mode((1080, 620))  # resizable not added
+screen_w, screen_h = pygame.display.get_surface().get_size()
+big_logo = pygame.image.load('UNDEAD UPRISING.png')
+
+
+def display_start_menu():
+    font = pygame.font.SysFont('consolas', 25)
+    screen.blit(big_logo, ((screen_w - 800) / 2, screen_h - 550))
+    instructions = font.render('Pick your character', True, (255, 255, 255))
+    screen.blit(instructions,
+                (screen_w / 2 - instructions.get_width() / 2, screen_h / 2 - instructions.get_height() / 2 - 100))
+    # mouse = pygame.mouse.get_pos()
+    border()
+    show_all_char()
+    start_button()
+    pygame.display.update()
+
+
+def start_button():
+    font = pygame.font.SysFont('consolas', 25, bold=True)
+    # pygame.draw.rect(screen, (255, 116, 63), [screen_w / 2 - 150 / 2 + 5, 515 + 5, 150, 40])
+    pygame.draw.rect(screen, (255, 0, 255), [screen_w / 2 - 150 / 2 - 5, 515 - 5, 160, 50])
+    pygame.draw.rect(screen, (0, 255, 255), [screen_w / 2 - 150 / 2, 515, 150, 40])
+    start_text = font.render('START', True, (0, 0, 0))
+    screen.blit(start_text,
+                (screen_w / 2 - 150 / 2 + start_text.get_width() / 2, 515 + start_text.get_height() / 2, 150, 40))
+
+
+def border():
+    pygame.draw.rect(screen, (255, 255, 255), [screen_w / 2 - 700 / 2, screen_h / 2 - 50, 700, 200])
+    pygame.draw.rect(screen, (0, 0, 0), [(screen_w / 2 - 350 + 25), screen_h / 2 - 25, 650, 150])
+
+
+# All Characters
+def show_all_char():
+    redchar()
+    orangechar()
+    yellowchar()
+    bluechar()
+    purplechar()
+    pinkchar()
+
+
+def redchar():
+    red_char = pygame.image.load('john.png')
+    screen.blit(red_char, (255, 325))
+
+
+def orangechar():
+    orange_char = pygame.image.load('swift.png')
+    screen.blit(orange_char, (355, 325))
+
+
+def yellowchar():
+    yellow_char = pygame.image.load('john.png')
+    screen.blit(yellow_char, (455, 325))
+
+
+def bluechar():
+    blue_char = pygame.image.load('swift.png')
+    screen.blit(blue_char, (555, 325))
+
+
+def purplechar():
+    purple_char = pygame.image.load('john.png')
+    screen.blit(purple_char, (655, 325))
+
+
+def pinkchar():
+    pink_char = pygame.image.load('swift.png')
+    screen.blit(pink_char, (755, 325))
