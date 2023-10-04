2b0ba3004cf0af4daf205d2271a918c7ab903e8c
Daniyaljuan <144381367+Daniyaljuan@users.noreply.github.com>
2023-10-04 15:02:37 -0400

    Update test.py
    pixilated transition

--- test.py
+++ test.py
@@ -1,31 +1,37 @@
-# importing pygame module
-import pygame
- 
-# importing sys module
-import sys
- 
-# initialising pygame
-pygame.init()
- 
-# creating display
-display = pygame.display.set_mode((500, 500))
- 
-# Creating the image surface
-image = pygame.image.load('STR.jpg')
- 
-# putting our image surface on display surface
-display.blit(image,(100,100))
- 
-# making the script wait for 5000 seconds
-pygame.time.wait(500)
- 
-# creating a running loop
-while True:
-    display.blit(image,(100,100))
-      # creating a loop to check events that are occurring
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    # updating the display
-    pygame.display.flip()
+import os
+import pygame as pg
+from random import *
+# It is used as a library
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = os.path.join(main_dir, "data")
+
+
+def show(image): # Shows surface
+    screen = pg.display.get_surface()
+    screen.fill((255, 255, 255))
+
+    screen.blit(image, (0, 0))
+    pg.display.flip()
+
+
+def main(): # Reacts on window closing.
+    pg.init()
+
+    pg.display.set_mode((255, 255))
+    surface = pg.Surface((255, 255))
+
+    pg.display.flip()
+    while 1:
+        for ev in pg.event.get():
+            if ev.type == pg.QUIT:
+                brk = 0
+                pg.display.quit()
+def animate(pixels): # Animation function.pixels is array of arrays as [x, y, (r,g,b)]
+    global surface
+    ar = pg.PixelArray(surface)
+    for x, y, color in pixels:
+        ar[x, y] = color
+    del ar
+    show(STR.jpg)
+main()
