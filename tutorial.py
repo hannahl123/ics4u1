338f44774eef5c809dfcabdbe2a37158ab1849be
Terinaly <146770208+Terinaly@users.noreply.github.com>
2023-10-16 15:41:39 -0400

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -4,7 +4,7 @@ import sys
 screen = pygame.display.set_mode((1080, 620))  # resizable not added
 screen_w, screen_h = pygame.display.get_surface().get_size()
 big_logo = pygame.image.load('images/backgrounds/Tutorial(TEST).png')
-size()
+
 def intro():
      screen.blit(big_logo, (0, 0))  # displays the background
 
